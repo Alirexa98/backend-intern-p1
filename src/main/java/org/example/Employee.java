7bb370e8bd6dc90423b8441b5ec85a800fe23209
Naserpour Alireza <anaserpoor98@gmail.com>
2022-07-03 09:38:20 +0430

    read from json and write to xml

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,249 @@
+package org.example;
+
+public class Employee {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private String about;
+    private String token;
+    private Object country;
+    private String location;
+    private Integer lng;
+    private Integer lat;
+    private String dob;
+    private Integer gender;
+    private Integer userType;
+    private Integer userStatus;
+    private String profilePicture;
+    private String coverPicture;
+    private Boolean enablefollowme;
+    private Boolean sendmenotifications;
+    private Boolean sendTextmessages;
+    private Boolean enabletagging;
+    private String createdAt;
+    private String updatedAt;
+    private Double livelng;
+    private Double livelat;
+    private String liveLocation;
+    private Integer creditBalance;
+    private Integer myCash;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Object getCountry() {
+        return country;
+    }
+
+    public void setCountry(Object country) {
+        this.country = country;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getLng() {
+        return lng;
+    }
+
+    public void setLng(Integer lng) {
+        this.lng = lng;
+    }
+
+    public Integer getLat() {
+        return lat;
+    }
+
+    public void setLat(Integer lat) {
+        this.lat = lat;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Integer getUserType() {
+        return userType;
+    }
+
+    public void setUserType(Integer userType) {
+        this.userType = userType;
+    }
+
+    public Integer getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(Integer userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getCoverPicture() {
+        return coverPicture;
+    }
+
+    public void setCoverPicture(String coverPicture) {
+        this.coverPicture = coverPicture;
+    }
+
+    public Boolean getEnablefollowme() {
+        return enablefollowme;
+    }
+
+    public void setEnablefollowme(Boolean enablefollowme) {
+        this.enablefollowme = enablefollowme;
+    }
+
+    public Boolean getSendmenotifications() {
+        return sendmenotifications;
+    }
+
+    public void setSendmenotifications(Boolean sendmenotifications) {
+        this.sendmenotifications = sendmenotifications;
+    }
+
+    public Boolean getSendTextmessages() {
+        return sendTextmessages;
+    }
+
+    public void setSendTextmessages(Boolean sendTextmessages) {
+        this.sendTextmessages = sendTextmessages;
+    }
+
+    public Boolean getEnabletagging() {
+        return enabletagging;
+    }
+
+    public void setEnabletagging(Boolean enabletagging) {
+        this.enabletagging = enabletagging;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Double getLivelng() {
+        return livelng;
+    }
+
+    public void setLivelng(Double livelng) {
+        this.livelng = livelng;
+    }
+
+    public Double getLivelat() {
+        return livelat;
+    }
+
+    public void setLivelat(Double livelat) {
+        this.livelat = livelat;
+    }
+
+    public String getLiveLocation() {
+        return liveLocation;
+    }
+
+    public void setLiveLocation(String liveLocation) {
+        this.liveLocation = liveLocation;
+    }
+
+    public Integer getCreditBalance() {
+        return creditBalance;
+    }
+
+    public void setCreditBalance(Integer creditBalance) {
+        this.creditBalance = creditBalance;
+    }
+
+    public Integer getMyCash() {
+        return myCash;
+    }
+
+    public void setMyCash(Integer myCash) {
+        this.myCash = myCash;
+    }
+
+}
