9bf843ed0e069ae24124072a662fd28da8c01eb3
Naserpour Alireza <anaserpoor98@gmail.com>
2022-07-04 09:50:53 +0430

    review and optimise code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,44 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Translating Started...");
-
-        var xmlMapper = new XmlMapper();
-        var objectMapper = new ObjectMapper();
-
-        List<Employee> result = new ArrayList<>(0);
-
-        try (var inputStream = Files.newInputStream(Paths.get("./EmployeeData.json"));
-             var outputStream = Files.newOutputStream(Paths.get("./EmployeeData.xml"),
-                     StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
-            result = objectMapper.readValue(inputStream, result.getClass());
-
-            System.out.println("Reading Done: Items Count -> " + result.size());
-
-            xmlMapper.writeValue(outputStream, result);
-
-            System.out.println("Writing Done!");
-        } catch (Exception e) {
-            System.out.println("Operation Failed!");
-            e.printStackTrace();
-        }
+  public static void main(String[] args) {
+    System.out.println("Translating Started...");
+    try {
+      List<Employee> result = decodeEmployeesFromJson(
+          Files.readString(Paths.get("./EmployeeData.json"))
+      );
+      System.out.println("Reading Done: Items Count -> " + result.size());
+      Files.writeString(
+          Paths.get("./EmployeeData.xml"),
+          encodeEmployeesToXml(result),
+          StandardOpenOption.CREATE,
+          StandardOpenOption.TRUNCATE_EXISTING
+      );
+      System.out.println("Writing Done!");
+    } catch (Exception e) {
+      System.out.println("Operation Failed!");
+      e.printStackTrace();
     }
+  }
+
+  static List<Employee> decodeEmployeesFromJson(String json) throws JsonProcessingException {
+    var objectMapper = new ObjectMapper();
+    return objectMapper.readValue(json, new TypeReference<List<Employee>>() {
+    });
+  }
+
+  static String encodeEmployeesToXml(List<Employee> employees) throws JsonProcessingException {
+    var xmlMapper = new XmlMapper();
+    return xmlMapper.writeValueAsString(employees);
+  }
 }
